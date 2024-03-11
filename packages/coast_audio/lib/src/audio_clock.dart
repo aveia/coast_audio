3c94d47da5ac8c7b19097bbb961ccdae4e518b1f
aveia <46282133+aveia@users.noreply.github.com>
2024-03-11 18:39:57 -0400

    fix the timekeeping in AudioIntervalClock

--- packages/coast_audio/lib/src/audio_clock.dart
+++ packages/coast_audio/lib/src/audio_clock.dart
@@ -22,7 +22,7 @@ class AudioIntervalClock extends AudioClock {
   AudioIntervalClock(this.interval);
 
   Timer? _timer;
-  double _elapsedTime = 0;
+  final Stopwatch _watch = Stopwatch();
 
   final Duration interval;
 
@@ -30,15 +30,15 @@ class AudioIntervalClock extends AudioClock {
   bool get isStarted => _timer != null;
 
   @override
-  AudioTime get elapsedTime => AudioTime(_elapsedTime);
+  AudioTime get elapsedTime => AudioTime(_watch.elapsedMicroseconds / 1000.0 / 1000.0);
 
   @override
   void start() {
     if (_timer != null) {
       stop();
     }
+    _watch.start();
     _timer = Timer.periodic(interval, (timer) {
-      _elapsedTime += interval.inMicroseconds / 1000 / 1000;
       for (var f in callbacks) {
         f(this);
       }
@@ -47,6 +47,7 @@ class AudioIntervalClock extends AudioClock {
 
   @override
   void stop() {
+    _watch.stop();
     _timer?.cancel();
     _timer = null;
   }
